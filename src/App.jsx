56847dd2a3204daf1a57b34d21cc286fa4527043
utsab043 <grgutsab66@gmail.com>
2023-07-03 11:59:19 +0545

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import CalculatorBtn from './Button';
 import { ScreenDisplay } from './Screen';
 
 // named export ma chai {} vitra rakhnu
-
+// hook
+// component ma matra use huncha and not inside normal function also always starts with use keyword
 export const App = () => {
   //let ScreenText = "99999";
   const [ScreenText ,setScreenText] = useState(0);
@@ -14,17 +15,28 @@ export const App = () => {
     console.log(ScreenText);
   }
   return (
-    <div> <screen text={ScreenText}/>
-    <CalculatorBtn text="0" handelclick={()=>DisplayText(0)}/>
-    <CalculatorBtn text="1" handelclick={()=>DisplayText(1)}/>
-    <CalculatorBtn text="2" handelclick={()=>DisplayText(2)}/>
-    <CalculatorBtn text="3" handelclick={()=>DisplayText(3)}/>
-    <CalculatorBtn text="4" handelclick={()=>DisplayText(4)}/>
-    <CalculatorBtn text="5" handelclick={()=>DisplayText(5)}/>
-    <CalculatorBtn text="6" handelclick={()=>DisplayText(6)}/>
-    <CalculatorBtn text="7" handelclick={()=>DisplayText(7)}/>
-    <CalculatorBtn text="8" handelclick={()=>DisplayText(8)}/>
-    <CalculatorBtn text="9" handelclick={()=>DisplayText(9)}/>
+    <div> 
+    <ScreenDisplay text={ScreenText}/>
+    <CalculatorBtn text="0" handelClick={()=>DisplayText(0)}/>
+    <CalculatorBtn text="1" handelClick={()=>DisplayText(1)}/>
+    <CalculatorBtn text="2" handelClick={()=>DisplayText(2)}/>
+    <CalculatorBtn text="3" handelClick={()=>DisplayText(3)}/>
+    <CalculatorBtn text="4" handelClick={()=>DisplayText(4)}/>
+    <CalculatorBtn text="5" handelClick={()=>DisplayText(5)}/>
+    <CalculatorBtn text="6" handelClick={()=>DisplayText(6)}/>
+    <CalculatorBtn text="7" handelClick={()=>DisplayText(7)}/>
+    <CalculatorBtn text="8" handelClick={()=>DisplayText(8)}/>
+    <CalculatorBtn text="9" handelClick={()=>DisplayText(9)}/>
+    <CalculatorBtn text="+" handelClick={()=>DisplayText("+")}/>
+    <CalculatorBtn text="-" handelClick={()=>DisplayText("-")}/>
+    <CalculatorBtn text="*" handelClick={()=>DisplayText("*")}/>
+    <CalculatorBtn text="/" handelClick={()=>DisplayText("/")}/>
+    <CalculatorBtn text="=" handelClick={()=>DisplayText("=")}/>
+    <CalculatorBtn text="<-" handelClick={()=>DisplayText("<-")}/>
+    <CalculatorBtn text="AC" handelClick={()=>DisplayText("AC")}/>
+    <CalculatorBtn text="." handelClick={()=>DisplayText(".")}/>
+    <CalculatorBtn text="(" handelClick={()=>DisplayText("(")}/>
+    <CalculatorBtn text=")" handelClick={()=>DisplayText(")")}/>
     </div>
   )
 }
