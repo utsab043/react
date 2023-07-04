0308ddb5ed4d44ad7a68dcdb5938284178093b36
utsab043 <grgutsab66@gmail.com>
2023-07-04 12:52:32 +0545

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,69 @@ import { ScreenDisplay } from './Screen';
 const Utility ={
 CLEAR : "CLEAR",
 BACK : "BACK",
+ADD : "+",
+SUB : "-",
+MUL : "*",
+DIVE : "/",
+PER :"%",
 }
 export const App = () => {
   const [ScreenText ,setScreenText] = useState("");
-       const DisplayText = (text) => {
-    if(text===Utility.CLEAR){
-      setScreenText("")
-    }
-    else if(text===Utility.BACK){
+  const [hasOperator, setHasOperator] = useState(false);
+  const DisplayText = (text) => {
+    if(text===Utility.CLEAR) {
+      setScreenText("");
+      return;
+    } 
+    
+    if(text===Utility.BACK) {
       setScreenText((prev) => {
-        return prev.slice(0, prev.length-1)
+        return prev.slice(0, prev.length-1);
       })
+      return;
+    } 
+
+    if(
+      (text === Utility.ADD
+      || text === Utility.SUB
+      || text === Utility.DIVE
+      || text === Utility.MUL
+      || text === Utility.PER
+      )
+      && !hasOperator
+    ) {
+      setHasOperator(true);
     }
-    else{
-    setScreenText((prev) => {
-    return prev+text;
-  });
-}
-}
+
+    if(
+      !(text === Utility.ADD
+      || text === Utility.SUB
+      || text === Utility.DIVE
+      || text === Utility.MUL
+      || text === Utility.PER
+      )
+      && hasOperator
+    ) {
+      setHasOperator(false)
+    }
+
+    if(
+      (text === Utility.ADD
+      || text === Utility.SUB
+      || text === Utility.DIVE
+      || text === Utility.MUL
+      || text === Utility.PER
+      )
+      && hasOperator
+    ) {
+      return;
+    }
+
+    setScreenText(prev => prev+text);
+  }
+  const showResult = () =>{
+  setScreenText(eval(ScreenText));
+  }
   return (
     <div className = "calculator">
     <h1>Calculator</h1>
@@ -34,33 +79,33 @@ export const App = () => {
     <div>
     <CalculatorBtn text="AC" handelClick={()=>DisplayText(Utility.CLEAR)}bgcolor="#ff000099"/>
     <CalculatorBtn text="DEL" handelClick={()=>DisplayText(Utility.BACK)}bgcolor="#00800099"/>
-    <CalculatorBtn text="%" handelClick={()=>DisplayText("%")}bgcolor="#00800099"/>
-    <CalculatorBtn text="/" handelClick={()=>DisplayText("/")}bgcolor="#00800099"/>
+    <CalculatorBtn text="%" handelClick={()=>DisplayText(Utility.PER)}bgcolor="#00800099"/>
+    <CalculatorBtn text="/" handelClick={()=>DisplayText(Utility.DIVE)}bgcolor="#00800099"/>
     </div>
     <div>
     <CalculatorBtn text="7" handelClick={()=>DisplayText(7)}/>
     <CalculatorBtn text="8" handelClick={()=>DisplayText(8)}/>
     <CalculatorBtn text="9" handelClick={()=>DisplayText(9)}/>
-    <CalculatorBtn text="*" handelClick={()=>DisplayText("*")}bgcolor="#00800099"/>
+    <CalculatorBtn text="*" handelClick={()=>DisplayText(Utility.MUL)}bgcolor="#00800099"/>
     </div>
     
     <div>
     <CalculatorBtn text="4" handelClick={()=>DisplayText(4)}/>
     <CalculatorBtn text="5" handelClick={()=>DisplayText(5)}/>
     <CalculatorBtn text="6" handelClick={()=>DisplayText(6)}/>
-    <CalculatorBtn text="-" handelClick={()=>DisplayText("-")}bgcolor="#00800099"/>
+    <CalculatorBtn text="-" handelClick={()=>DisplayText(Utility.SUB)}bgcolor="#00800099"/>
     </div>
     <div>
     <CalculatorBtn text="1" handelClick={()=>DisplayText(1)}/>
     <CalculatorBtn text="2" handelClick={()=>DisplayText(2)}/>
     <CalculatorBtn text="3" handelClick={()=>DisplayText(3)}/>
-    <CalculatorBtn text="+" handelClick={()=>DisplayText("+")}bgcolor="#00800099"/>
+    <CalculatorBtn text="+" handelClick={()=>DisplayText(Utility.ADD)}bgcolor="#00800099"/>
     </div>
     <div>
     <CalculatorBtn text="00" handelClick={()=>DisplayText("00")}/>
     <CalculatorBtn text="0" handelClick={()=>DisplayText(0)}/>
     <CalculatorBtn text="." handelClick={()=>DisplayText(".")}/>
-    <CalculatorBtn text="=" handelClick={()=>DisplayText("=")}bgcolor="#FFA50099"/>
+    <CalculatorBtn text="=" handelClick={showResult}bgcolor="#FFA50099"/>
     </div>
     </div>
   )
